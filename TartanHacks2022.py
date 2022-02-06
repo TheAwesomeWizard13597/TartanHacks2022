203db7c136b980aaaebca0aedf975aecb57b8ce5
RadWizard64 <80139492+RadWizard64@users.noreply.github.com>
2022-02-06 09:50:05 -0500

    Add files via upload

--- TartanHacks2022.py
+++ TartanHacks2022.py
@@ -6,12 +6,17 @@ from tkinter import *
 def show_values():
     print (w.get())
 
+# Initialize root
 root = Tk()
+
+# Add scaler and refresh button
+w = Scale(root, from_=0, to=100, orient=HORIZONTAL, length=500)
+w.pack()
+Button(root, text='Refresh', command=show_values).pack()
+
+# Add canvas
 canvas = tkinter.Canvas(root, height=400, width=600)
 canvas.configure(bd=0, highlightthickness=0)
 canvas.pack()
-w = Scale(root, from_=0, to=100, orient=HORIZONTAL, length=500)
-w.pack()
-Button(root, text='Show', command=show_values).pack()
 
 root.mainloop()
